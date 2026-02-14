eec1cfd156d2ae7770418a0c431c8062dc040bda
adriasc <42960713+adriasc@users.noreply.github.com>
2026-02-14 11:15:10 +0800

    Add files via upload

--- app-bar.js
+++ app-bar.js
@@ -1,19 +1,12 @@
 const ISO_VALUES = [25, 50, 100, 125, 160, 200, 250, 320, 400, 500, 640, 800, 1000, 1250, 1600, 3200];
-const APP_VERSION = "1.5.0";
+const APP_VERSION = "1.6.0";
 const APERTURES = [1.4, 2.0, 2.8, 4.0, 5.6, 8.0, 11.0, 16.0];
-const SHUTTERS = [
-  1 / 2000,
-  1 / 1000,
-  1 / 500,
-  1 / 250,
-  1 / 125,
-  1 / 60,
-  1 / 30,
-  1 / 15,
-  1 / 8,
-  1 / 4,
-  1 / 2,
-  1
+const SHUTTERS = [1 / 2000, 1 / 1000, 1 / 500, 1 / 250, 1 / 125, 1 / 60, 1 / 30, 1 / 15, 1 / 8, 1 / 4, 1 / 2, 1];
+const RULER_SHUTTERS = [
+  1 / 8000, 1 / 6400, 1 / 5000, 1 / 4000, 1 / 3200, 1 / 2500, 1 / 2000, 1 / 1600, 1 / 1250, 1 / 1000,
+  1 / 800, 1 / 640, 1 / 500, 1 / 400, 1 / 320, 1 / 250, 1 / 200, 1 / 160, 1 / 125, 1 / 100, 1 / 80,
+  1 / 60, 1 / 50, 1 / 40, 1 / 30, 1 / 25, 1 / 20, 1 / 15, 1 / 13, 1 / 10, 1 / 8, 1 / 6, 1 / 5, 1 / 4,
+  1 / 3, 1 / 2.5, 1 / 2, 1 / 1.6, 1 / 1.3, 1
 ];
 
 const GRID_ROWS = 5;
@@ -37,7 +30,7 @@ const startBtn = document.getElementById("startBtn");
 const evReadout = document.getElementById("evReadout");
 const appVersion = document.getElementById("appVersion");
 const isoSelect = document.getElementById("isoSelect");
-const pairBar = document.getElementById("pairBar");
+const exposureRuler = document.getElementById("exposureRuler");
 const cameraWrap = document.getElementById("cameraWrap");
 
 let selectedISO = 400;
@@ -65,13 +58,13 @@ function init() {
   renderTapMarker();
   createZoneCells();
   paintReferenceCell();
-  renderPairs(smoothedEV);
+  renderRuler(smoothedEV);
 
   startBtn.addEventListener("click", startCamera);
   cameraWrap.addEventListener("click", onCameraTap);
   isoSelect.addEventListener("change", () => {
     selectedISO = Number(isoSelect.value);
-    renderPairs(smoothedEV);
+    renderRuler(smoothedEV);
   });
 }
 
@@ -192,13 +185,13 @@ function meterFrame() {
   smoothedEV = blend(smoothedEV, rawEV, EV_SMOOTHING);
 
   evReadout.textContent = smoothedEV.toFixed(1);
-  renderPairs(smoothedEV);
+  renderRuler(smoothedEV);
 }
 
-function renderPairs(ev100) {
-  pairBar.innerHTML = APERTURES.map((ap) => {
-    const shutter = shutterSeconds(ev100, selectedISO, ap);
-    return `<span class="pair-chip"><span class="pair-left">f/${ap.toFixed(1)}</span><span class="pair-sep">|</span><span class="pair-right">${formatShutter(shutter)}</span></span>`;
+function renderRuler(ev100) {
+  exposureRuler.innerHTML = RULER_SHUTTERS.map((shutter) => {
+    const ap = apertureFor(ev100, selectedISO, shutter);
+    return `<div class="ruler-col"><div class="ruler-aperture">f/${ap.toFixed(1)}</div><div class="ruler-tick"></div><div class="ruler-shutter">${formatShutter(shutter)}</div></div>`;
   }).join("");
 }
 
